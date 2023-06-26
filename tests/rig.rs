9b485f1b51f32b5d74d3394f7c4a309f4b1d9a74
max <max@karl.wf>
2023-06-26 12:05:25 +0200

    Add ignored test for ICOM IC7200

--- tests/rig.rs
+++ tests/rig.rs
@@ -71,3 +71,24 @@ fn rig_mode() {
         assert_eq!(pb_after, 1234);
     })
 }
+
+#[test]
+#[ignore]
+fn device_icom_ic7200() {
+    tokio!({
+        let mut d = Daemon::default()
+            .set_model(3061)
+            .set_serial_speed(19200)
+            .set_civ_address(0x76)
+            .set_rig_file("/dev/ttyUSB0".into());
+        d.spawn().unwrap();
+
+        sleep(Duration::from_millis(250)).await;
+
+        let mut rig = Rig::new("127.0.0.1", 4532);
+        rig.connect().await.unwrap();
+
+        rig.get_frequency().await.unwrap();
+        rig.get_mode().await.unwrap();
+    })
+}
