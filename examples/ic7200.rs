0cb71d26a59b0a2e85b663b2e048e07941317256
max <max@karl.wf>
2023-07-02 21:25:22 +0200

    Add example for ICOM IC7200

---
+++ examples/ic7200.rs
@@ -0,0 +1,23 @@
+use rigctld::{Daemon, Rig};
+use tokio::time::{sleep, Duration};
+
+#[tokio::main]
+async fn main() {
+    let mut d = Daemon::default()
+        .set_model(3061)
+        .set_serial_speed(19200)
+        .set_civ_address(0x76)
+        .set_rig_file("/dev/ttyUSB0".into());
+    d.spawn().unwrap();
+
+    sleep(Duration::from_millis(1000)).await;
+
+    let mut rig = Rig::new("127.0.0.1", 4532);
+    rig.set_communication_timeout(Duration::from_millis(1000));
+    rig.connect().await.unwrap();
+
+    loop {
+        rig.get_frequency().await.unwrap();
+        rig.get_mode().await.unwrap();
+    }
+}
